77339ebece736bf6adf12e8048be708e1c7d0d92
Lương Đức Quang Trung <quangtrung54677@gmail.com>
2025-06-28 14:05:39 +0700

    join stream and show camera on display

--- Starter Template/js/room_rtc.js
+++ Starter Template/js/room_rtc.js
@@ -17,5 +17,21 @@ if(!roomId){
     roomId = 'main'
 }
 
-let localTrack = []
-let remoteTrack = {}
+let localTracks = []
+let remoteTracks = {}
+
+let joinRoomInit = async() =>{
+    client = AgoraRTC.createClient({ mode: 'rtc', codec: 'vp8' });
+    await client.join(APP_ID,roomId,token,uid)
+    joinStream()
+}
+let joinStream = async() =>{
+    localTracks = await AgoraRTC.createMicrophoneAndCameraTracks()
+    
+    let player = `<div class="video__container" id="user-container-${uid}">
+                        <h1 class= "video-player" id = "user-${uid}"></h1>
+                </div>`
+    document.getElementById('streams__container').insertAdjacentHTML('beforeend', player);
+    localTracks[1].play(`user-${uid}`)
+}
+joinRoomInit()
